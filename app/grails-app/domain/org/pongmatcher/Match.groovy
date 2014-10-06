8ec7983a568904d62c019c5115a9513a0eaae75b
Andrew Bruce <andrew@camelpunch.com>
2014-10-06 10:36:46 +0100

    add (currently optional) proposedDate to Match

--- app/grails-app/domain/org/pongmatcher/Match.groovy
+++ app/grails-app/domain/org/pongmatcher/Match.groovy
@@ -3,4 +3,9 @@ package org.pongmatcher
 class Match {
     MatchRequest matchRequest1
     MatchRequest matchRequest2
+    Date proposedDate
+
+    static constraints = {
+        proposedDate blank: true
+    }
 }
