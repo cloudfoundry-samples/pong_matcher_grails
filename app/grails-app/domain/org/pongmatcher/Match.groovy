af956ca3b75fb806a622231345868477e93f2c5f
Andrew Bruce <andrew@camelpunch.com>
2014-10-07 17:38:50 +0100

    remove proposed date from schema

--- app/grails-app/domain/org/pongmatcher/Match.groovy
+++ app/grails-app/domain/org/pongmatcher/Match.groovy
@@ -3,11 +3,6 @@ package org.pongmatcher
 class Match {
     MatchRequest matchRequest1
     MatchRequest matchRequest2
-    Date proposedDate
-
-    static constraints = {
-        proposedDate blank: true, nullable: true
-    }
 
     static mapping = {
         table "`match`"
--- app/grails-app/migrations/changelog.groovy
+++ app/grails-app/migrations/changelog.groovy
@@ -1,6 +1,6 @@
 databaseChangeLog = {
 
-	changeSet(author: "pivotal (generated)", id: "1412698834675-1") {
+	changeSet(author: "pivotal (generated)", id: "1412699223280-1") {
 		createTable(tableName: "match") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "matchPK")
@@ -17,12 +17,10 @@ databaseChangeLog = {
 			column(name: "match_request2_id", type: "varchar(255)") {
 				constraints(nullable: "false")
 			}
-
-			column(name: "proposed_date", type: "datetime")
 		}
 	}
 
-	changeSet(author: "pivotal (generated)", id: "1412698834675-2") {
+	changeSet(author: "pivotal (generated)", id: "1412699223280-2") {
 		createTable(tableName: "match_request") {
 			column(name: "uuid", type: "varchar(255)") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "match_requestPK")
@@ -38,7 +36,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "pivotal (generated)", id: "1412698834675-3") {
+	changeSet(author: "pivotal (generated)", id: "1412699223280-3") {
 		createTable(tableName: "result") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "resultPK")
@@ -58,23 +56,23 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "pivotal (generated)", id: "1412698834675-6") {
+	changeSet(author: "pivotal (generated)", id: "1412699223280-6") {
 		createIndex(indexName: "FK_aavvm3veobpacpnqd8o3gr775", tableName: "match") {
 			column(name: "match_request1_id")
 		}
 	}
 
-	changeSet(author: "pivotal (generated)", id: "1412698834675-7") {
+	changeSet(author: "pivotal (generated)", id: "1412699223280-7") {
 		createIndex(indexName: "FK_lkbnrcv1u8ni17f5qr30b1ogs", tableName: "match") {
 			column(name: "match_request2_id")
 		}
 	}
 
-	changeSet(author: "pivotal (generated)", id: "1412698834675-4") {
+	changeSet(author: "pivotal (generated)", id: "1412699223280-4") {
 		addForeignKeyConstraint(baseColumnNames: "match_request1_id", baseTableName: "match", constraintName: "FK_aavvm3veobpacpnqd8o3gr775", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "uuid", referencedTableName: "match_request", referencesUniqueColumn: "false")
 	}
 
-	changeSet(author: "pivotal (generated)", id: "1412698834675-5") {
+	changeSet(author: "pivotal (generated)", id: "1412699223280-5") {
 		addForeignKeyConstraint(baseColumnNames: "match_request2_id", baseTableName: "match", constraintName: "FK_lkbnrcv1u8ni17f5qr30b1ogs", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "uuid", referencedTableName: "match_request", referencesUniqueColumn: "false")
 	}
 }
