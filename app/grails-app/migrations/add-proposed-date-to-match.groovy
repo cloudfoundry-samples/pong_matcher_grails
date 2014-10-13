34cf7f7ef4521379f6dc5f6dc4cefd4382aa92c0
Andrew Bruce <andrew@camelpunch.com>
2014-10-13 10:05:20 +0100

    Revert "re-adding proposed date with a migration"
    This reverts commit ae01aa1afee644cae2c7c7b95894eb32968b9525.

--- app/grails-app/migrations/add-proposed-date-to-match.groovy
+++
@@ -1,8 +0,0 @@
-databaseChangeLog = {
-
-	changeSet(author: "pivotal (generated)", id: "1412700272576-1") {
-		addColumn(tableName: "match") {
-			column(name: "proposed_date", type: "datetime")
-		}
-	}
-}
