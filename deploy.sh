d0010d595cd747949ea730575edc840110317ff7
Andrew Bruce <andrew@camelpunch.com>
2014-10-16 11:40:47 +0100

    Add presumably missing =

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-docker run -e "CF_HOME /pong_matcher_grails" -t docker.gocd.cf-app.com:5000/pong-matcher-grails /bin/bash -c "\
+docker run -e "CF_HOME=/pong_matcher_grails" -t camelpunch/pong-matcher-grails /bin/bash -c "\
     cd pong_matcher_grails
     cf api https://api.run.pivotal.io &&
     cf auth $CF_USERNAME $CF_PASSWORD &&
