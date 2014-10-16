947e979ebcbba1a087b92802d3db87a4bbbb988c
Andrew Bruce <andrew@camelpunch.com>
2014-10-16 13:21:12 +0000

    Attempt to pull image before building

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+set -ex
+
+docker pull docker.gocd.cf-app.com:5000/pong-matcher-grails || true
 docker build -t docker.gocd.cf-app.com:5000/pong-matcher-grails .
 docker push docker.gocd.cf-app.com:5000/pong-matcher-grails
