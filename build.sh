2aa1b11820425798d42fc196cedd65dddac8e27b
Andrew Bruce <andrew@camelpunch.com>
2014-11-11 11:31:06 +0000

    Allow insertion of docker build options

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 set -ex
 
 docker pull docker.gocd.cf-app.com:5000/pong-matcher-grails || true
-docker build -t docker.gocd.cf-app.com:5000/pong-matcher-grails .
+docker build $DOCKER_BUILD_OPTS -t docker.gocd.cf-app.com:5000/pong-matcher-grails .
 docker push docker.gocd.cf-app.com:5000/pong-matcher-grails
